12a39fb4484628bcd7976e07e57fcb17886187f3
Eric Conklin <eric.s.conklin@gmail.com>
2014-09-01 19:59:33 -0400

    first

---
+++ avlminimap.js
@@ -0,0 +1,271 @@
+var avlminimap2 = (function(){
+	var minimap = {}
+
+	var uniqueGroupID = 0;
+
+	function createUniqueGroupID() {
+		return 'group-'+uniqueGroupID++;
+	}
+
+	minimap.Map = function() {
+		var svg,
+			width = window.innerWidth,
+			height = window.innerHeight,
+			groups,
+			paths,
+			layerCache = {},
+			collection = { type: 'FeatureCollection', features: [] },
+			projection = d3.geo.albers(),
+			path = d3.geo.path().projection(projection);
+
+		function map(selection) {
+			svg = selection
+				.attr('class', 'avl-minimap-map')
+				.attr('width', width)
+				.attr('height', height);
+		}
+
+		map.width = function(w) {
+			if (!arguments.length) {
+				return width;
+			}
+			width = w;
+			return map;
+		}
+		map.height = function(h) {
+			if (!arguments.length) {
+				return height;
+			}
+			height = h;
+			return map;
+		}
+		map.projection = function(p) {
+			if (!arguments.length) {
+				return projection;
+			}
+			projection = p;
+			return map;
+		}
+		map.path = function(p) {
+			if (!arguments.length) {
+				return path;
+			}
+			path = p;
+			return map;
+		}
+		map.draw = function() {
+			for (var id in layerCache) {
+				layerCache[id].draw();
+			}
+		}
+		map.update = function() {
+			paths.attr('d', path);
+			return map;
+		}
+		map.transition = function() {
+			paths.transition()
+				.duration(250)
+				.attr('d', path);
+			return map;
+		}
+		map.reset = function() {
+			return map.zoomToBounds(collection).transition();
+		}
+		map.append = function(l) {
+			var id = createUniqueGroupID();
+
+			layerCache[id] = l;
+
+			l.data().forEach(function(d) {
+				collection.features = collection.features.concat(d.features);
+			})
+
+			svg.append('g').attr('id', id)
+				.attr('class', 'avl-minimap-group')
+				.call(l, map, svg, id);
+
+			groups = svg.selectAll('.avl-minimap-group');
+
+			map.zoomToBounds(collection);
+			map.draw();
+
+			paths = groups.selectAll('path');
+
+			return map;
+		}
+
+	    map.zoomToBounds = function(collection) {
+	        var bounds = path.bounds(collection),
+	            wdth = bounds[1][0] - bounds[0][0],
+	            hght = bounds[1][1] - bounds[0][1],
+
+	            k = Math.min(width/wdth, height/hght),
+	            scale = projection.scale()*k*0.95;
+
+	        projection.scale(scale);
+
+	        bounds = path.bounds(collection);
+
+	        var centroid = [(bounds[1][0]+bounds[0][0])/2, (bounds[1][1]+bounds[0][1])/2],//path.centroid(collection),
+	            translate = projection.translate();
+
+	        projection.translate([translate[0] - centroid[0] + width / 2,
+	                             translate[1] - centroid[1] + height / 2]);
+
+	        return map;
+	    }
+
+		return map;
+	}
+
+	minimap.Layer = function() {
+		var data = [],
+			groups,
+			layerID,
+			group,
+			projection,
+			path,
+			onClick = null,
+			activated = null,
+			clickBack = null,
+			svg,
+			map;
+
+		function layer(selection, _map, _svg, id) {
+			if (selection) {
+				group = selection;
+				svg = _svg;
+				layerID = id;
+				map = _map;
+				projection = map.projection();
+				path = map.path();
+			}
+
+			groups = group.selectAll('g')
+				.data(data);
+
+			groups.exit().remove();
+
+			groups.enter().append('g');
+		}
+
+		layer.data = function(d) {
+			if (!arguments.length) {
+				return data;
+			}
+			data.push(d);
+			return layer;
+		}
+		layer.draw = function() {
+			groups.selectAll('path')
+				.data(function(d) { return d.features; })
+				.enter().append('path')
+				.attr('class', 'avl-minimap-path')
+				.attr('d', path)
+				.style(styles)
+				.on('click.avl-minimap-click', onClick);
+		}
+		layer.styles = function(s) {
+			if (!arguments.length) {
+				return styles;
+			}
+			styles = s;
+			return layer;
+		}
+		layer.onClick = function(arg, cb) {
+			switch(arg) {
+				case 'zoom':
+					onClick = zoom;
+					break;
+				case 'popout':
+					onClick = popout;
+					break;
+			}
+			if (cb) {
+				clickBack = cb;
+			}
+			return layer;
+		}
+
+		function zoom(d) {
+			if (activated == this) {
+				activated = null;
+				map.reset();
+				return;
+			}
+
+			activated = this;
+			map.zoomToBounds(d);
+
+			map.transition();
+		}
+
+		function popout(d) {
+			if (activated) {
+				return;
+			}
+
+			activated = this;
+
+			var p = d3.select(this);
+
+			var temp = d3.select(svg.node().appendChild(this.cloneNode()))
+				.datum(d)
+				.attr('d', path)
+				.on('click', unpopout);
+
+			var allPaths = svg.selectAll('path')
+				.filter(function() { return this != temp.node(); })
+
+			allPaths.transition()
+				.duration(250)
+				.style('opacity', 0.25);
+
+			p.style('display', 'none');
+
+			var savedTranslate = projection.translate(),
+				savedScale = projection.scale();
+
+			map.zoomToBounds(d);
+
+	        temp.transition()
+	        	.duration(250)
+	        	.attr('d', path)
+	        	.each('end', function() {
+			        if (clickBack) {
+			        	clickBack.bind(activated)(d);
+			        }
+	        	});
+
+
+	        function unpopout() {
+		        projection.scale(savedScale)
+		        	.translate(savedTranslate);
+
+    			if (clickBack) {
+    				clickBack.bind(activated)(d);
+    			}
+
+	        	allPaths.transition()
+	        		.duration(250)
+	        		.style('opacity', 1.0)
+	        		.each('end', function() {
+	        			d3.select(this).style('opacity', null);
+	        		})
+
+	        	temp.transition()
+	        		.duration(250)
+	        		.attr('d', path)
+	        		.each('end', function() {
+	        			activated = null;
+	        			d3.select(this).remove();
+	        			p.style('display', null);
+	        		});
+	        }
+		}
+
+		return layer;
+	}
+
+	return minimap;
+})()
---
+++ myJS.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+	var menubar = avlmenu.Menubar();
+
+	d3.select('#menu').call(menubar);
+
+	var tab = avlmenu.Tab()
+		.data([{ text: 'Popout', id: '#popout' }, { text: 'Zoom', id: '#zoom' }]);
+
+	menubar.append(tab);
+
+	var popoutMap = avlminimap2.Map()
+		.width(window.innerWidth)
+		.height(window.innerHeight-30);
+
+	d3.select('#popout').append('svg')
+		.call(popoutMap);
+
+	var zoomMap = avlminimap2.Map()
+		.width(window.innerWidth)
+		.height(window.innerHeight-30);
+
+	d3.select('#zoom').append('svg')
+		.call(zoomMap);
+
+	d3.json('./us_states.json', function(error, data) {
+		var json = topojson.feature(data, data.objects.states);
+
+		json.features.sort(function(a, b) { return d3.geo.area(a)-d3.geo.area(b); });
+
+		var fill = d3.scale.linear()
+			.domain([0, json.features.length-1])
+			.range(['#fdd', '#800'])
+
+		var popoutLayer = avlminimap2.Layer()
+			.data(json)
+			.onClick('popout', clicked)
+			.styles({ fill: function(d, i) { return fill(i); }, stroke: '#300' });
+
+		popoutMap.append(popoutLayer);
+
+		var zoomLayer = avlminimap2.Layer()
+			.data(json)
+			.onClick('zoom', clicked)
+			.styles({ fill: function(d, i) { return fill(i); }, stroke: '#300' });
+
+		zoomMap.append(zoomLayer);
+	})
+
+	function clicked(d) {
+		console.log(d);
+	}
+}
